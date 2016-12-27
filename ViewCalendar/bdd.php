10a3227be7c3ed4f2fee1d3d83834846b977f5d0
mandeep-vratesh <mandeep.vratesh@gmail.com>
2016-12-27 18:58:56 +0530

    modifications for using remote database

--- ViewCalendar/bdd.php
+++ ViewCalendar/bdd.php
@@ -1,7 +1,11 @@
 <?php
 try
 {
+	//to work on the local database
 	$bdd = new PDO('mysql:host=localhost;dbname=rait_calendar_sys', 'root', '');
+
+	//to work on the remote database
+	// $bdd = new PDO('mysql:host=sql6.freesqldatabase.com;dbname=sql6151113', 'sql6151113', 'VlFmqI5kJ4');
 }
 catch(Exception $e)
 {
